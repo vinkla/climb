595ef73fe23dfc2bd2aa74e5f209dc7730a5714b
Vincent Klaiber <vincentklaiber@gmail.com>
2015-11-24 17:49:58 +0100

    Add packagist test

---
+++ tests/PackagistTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of Climb.
+ *
+ * (c) Vincent Klaiber <hello@vinkla.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Vinkla\Tests\Climb;
+
+use Mockery;
+use Vinkla\Climb\Packagist;
+
+/**
+ * This is the Packagist test class.
+ *
+ * @author Vincent Klaiber <vincent@schimpanz.com>
+ */
+class PackagistTest extends AbstractTestCase
+{
+    public function testGetLatestVersion()
+    {
+        $packagist = Mockery::mock(Packagist::class);
+
+        $packagist->shouldReceive('getLatestVersion')
+            ->with('vinkla/climb')
+            ->andReturn('1.0.0');
+
+        $this->assertEquals('1.0.0', $packagist->getLatestVersion('vinkla/climb'));
+    }
+}
