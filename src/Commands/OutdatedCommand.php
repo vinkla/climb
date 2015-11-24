ec8aeaef54c720769b0e9279ca4713e9f6505db8
Vincent Klaiber <vincentklaiber@gmail.com>
2015-11-24 17:55:50 +0100

    Return correct types

--- src/Commands/OutdatedCommand.php
+++ src/Commands/OutdatedCommand.php
@@ -50,7 +50,7 @@ class OutdatedCommand extends Command
      * @param \Symfony\Component\Console\Input\InputInterface $input
      * @param \Symfony\Component\Console\Output\OutputInterface $output
      *
-     * @return mixed
+     * @return int
      */
     public function execute(InputInterface $input, OutputInterface $output)
     {
--- src/Commands/UpdateCommand.php
+++ src/Commands/UpdateCommand.php
@@ -59,7 +59,7 @@ class UpdateCommand extends Command
      * @param \Symfony\Component\Console\Input\InputInterface $input
      * @param \Symfony\Component\Console\Output\OutputInterface $output
      *
-     * @return mixed
+     * @return int
      */
     public function execute(InputInterface $input, OutputInterface $output)
     {
