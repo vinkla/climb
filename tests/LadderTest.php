98298e62a8ed995a611f322a46fe9925b1090f55
Vincent Klaiber <vincentklaiber@gmail.com>
2015-11-26 09:35:44 +0100

    Add basic ladder test

---
+++ tests/LadderTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of Climb.
+ *
+ * (c) Vincent Klaiber <hello@vinkla.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Vinkla\Tests\Climb;
+
+use Vinkla\Climb\Ladder;
+
+/**
+ * This is the ladder test class.
+ *
+ * @author Vincent Klaiber <hello@vinkla.com>
+ */
+class LadderTest extends AbstractTestCase
+{
+    public function testGetOutdatedPackages()
+    {
+        $ladder = new Ladder();
+        $packages = $ladder->getOutdatedPackages();
+        $this->assertTrue(is_array($packages));
+    }
+}
