df15315694c89766ba5c23a52d52dd0e2e1aeb25
Vincent Klaiber <vincentklaiber@gmail.com>
2015-12-08 16:53:10 +0100

    Update version test

--- tests/VersionTest.php
+++ tests/VersionTest.php
@@ -35,12 +35,7 @@ class VersionTest extends AbstractTestCase
 
     public function testLatest()
     {
-        $versions = [
-            'v2.0.1',
-            '3.1',
-            '3.1.5',
-            '3.2.0-dev',
-        ];
+        $versions = ['v2.0.1', '3.1', '3.1.5', '3.2.0-dev'];
 
         $this->assertSame('3.1.5', Version::latest($versions));
     }
